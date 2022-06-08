a098395c14e0d98df7e5dcbe92a6879df69b9440
kagisowilly <mphayiw@gmail.com>
2022-06-08 13:01:58 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let letters = "";
+
+function keyLetter(letterr){
+    letters = letters + letterr;
+    document.getElementById("output").innerText =letters;
+};
+
+function clearOutput(){
+    letters = "";
+    document.getElementById("output").innerText = letters;
+};
+
+function spaceOutput(){
+    letters = letters + "  ";
+    document.getElementById("output").innerText = letters;
+};
+
+let sendMessage = () =>{
+    alert("Message sent successfully")
+}
